337f6e571e211a7bfa817dbc7a92b554218274a4
Zilan Hasnath Lithon <zilanhasnath@gmail.com>
2025-11-21 18:51:29 +0600

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <div className='pt-10'>
-            <div className='bottom-0 bg-white shadow-2xl py-9 w-full text-[13px]'>
-                <h1 className='flex justify-center'>yesgram © 2025</h1>
-                <ul className='flex flex-row gap-5 justify-center mt-2'>
+        <div className="pt-10">
+            <div className="bottom-0 bg-white shadow-2xl py-9 w-full text-[13px]">
+                <h1 className="flex justify-center">yesgram © 2025</h1>
+
+                <ul className="flex flex-wrap gap-5 justify-center mt-2 px-4 text-center">
                     <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/privacy-policy">Privacy Policy</Link></li>
                     <li><Link to="/terms">Terms and Conditions</Link></li>
                     <li><Link to="/faq">FAQ</Link></li>
                     <li><Link to="/sitemap">Site Map</Link></li>
                 </ul>
-
             </div>
         </div>
-
-
     )
 }
 
